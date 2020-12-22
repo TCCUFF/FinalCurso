afb7ee7f0dfa090af32a10e1035c16afa2886da8
Lucas Vieira <lucasvs@id.uff.br>
2020-12-21 21:48:31 -0300

    Parenteses fechado

--- conectarBanco.js
+++ conectarBanco.js
@@ -21,7 +21,7 @@ app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
 app.set('views', __dirname + '/views');
 
-app.use(express.static(path.join(__dirname, 'public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extend:false }));
